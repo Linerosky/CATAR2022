5cf15d929479be8a5cd1bd566744064cc98d2e5d
Linerosky <daniellinerohd@gmail.com>
2022-11-14 22:37:13 -0500

    First steps - config

--- functions.js
+++ functions.js
@@ -1,11 +1,11 @@
-function sendT() {
+function config() {
     var fechaini = "0";
     var horaini = "0";
     var fechafin = "0";
     var horafin = "0";
 
     $.ajax({
-        url: 'datadb.php',
+        url: 'config.php',
         type: 'post',
         data: {
             fechaini: fechaini,
@@ -19,3 +19,65 @@ function sendT() {
     })
 }
 
+function online() {
+    var fechaini = "0";
+    var horaini = "0";
+    var fechafin = "0";
+    var horafin = "0";
+
+    $.ajax({
+        url: 'on-line.php',
+        type: 'post',
+        data: {
+            fechaini: fechaini,
+            fechafin: fechafin,
+            horaini: horaini,
+            horafin: horafin
+        }
+
+    }).done(function (res) {
+        data = JSON.parse(res);
+    })
+}
+
+function program() {
+    var fechaini = "0";
+    var horaini = "0";
+    var fechafin = "0";
+    var horafin = "0";
+
+    $.ajax({
+        url: 'program.php',
+        type: 'post',
+        data: {
+            fechaini: fechaini,
+            fechafin: fechafin,
+            horaini: horaini,
+            horafin: horafin
+        }
+
+    }).done(function (res) {
+        data = JSON.parse(res);
+    })
+}
+
+function visorweb() {
+    var fechaini = "0";
+    var horaini = "0";
+    var fechafin = "0";
+    var horafin = "0";
+
+    $.ajax({
+        url: 'visor-web.php',
+        type: 'post',
+        data: {
+            fechaini: fechaini,
+            fechafin: fechafin,
+            horaini: horaini,
+            horafin: horafin
+        }
+
+    }).done(function (res) {
+        data = JSON.parse(res);
+    })
+}
